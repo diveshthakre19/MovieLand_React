90e32f5ea03cf6697184f59a8f1370394a708950
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-27 20:27:45 +0530

    search movie function using useEffect

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const API_URL = "http://www.omdbapi.com?apikey=db723afc7";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    searchMovies("Batman");
+  }, []);
   return <h1>App</h1>;
 };
 
