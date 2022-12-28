add1e0d49b2102b89d3f9014fe01f4094700bbaf
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-28 19:17:30 +0530

    created temp  movie card

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./App.css";
-const API_URL = "http://www.omdbapi.com?apikey=db723afc7";
+import SearchIcon from "./search.svg";
+const API_URL = "http://www.omdbapi.com?apikey=b723afc7";
 
 const App = () => {
   const searchMovies = async (title) => {
@@ -8,10 +9,54 @@ const App = () => {
     const data = await response.json();
     console.log(data);
   };
+
+  const movie1 = {
+    Title: "The Batman",
+    Year: "2022",
+    imdbID: "tt1877830",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg",
+  };
+
   useEffect(() => {
     searchMovies("Batman");
   }, []);
-  return <h1>App</h1>;
+
+  return (
+    <div className="app">
+      <h1>Movieland</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+      <div className="conatainer">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={
+                movie1.Poster !== "N/A"
+                  ? movie1.Poster
+                  : "https://via.placeholder.com/400"
+              }
+              alt={movie1.title}
+            />
+          </div>
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
