946f1e4adc706bdeebf8e48cd475e2d74a68ae3f
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-28 23:02:52 +0530

    almost done worked on movieCard

--- src/app.js
+++ src/app.js
@@ -1,26 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
-const API_URL = "http://www.omdbapi.com?apikey=b723afc7";
 
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=b723afc7";
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSerchTerm] = useState(" ");
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data);
-  };
-
-  const movie1 = {
-    Title: "The Batman",
-    Year: "2022",
-    imdbID: "tt1877830",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg",
+    setMovies(data.Search);
   };
 
   useEffect(() => {
-    searchMovies("Batman");
+    searchMovies("spiderman");
   }, []);
 
   return (
@@ -29,32 +24,27 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSerchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies({ searchTerm })}
+        />
       </div>
-      <div className="conatainer">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-          <div>
-            <img
-              src={
-                movie1.Poster !== "N/A"
-                  ? movie1.Poster
-                  : "https://via.placeholder.com/400"
-              }
-              alt={movie1.title}
-            />
-          </div>
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
+
+      {movies.length > 0 ? (
+        <div className="conatainer">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-      </div>
+      ) : (
+        <div className="empty">
+          <h1>No movie Found</h1>
+        </div>
+      )}
     </div>
   );
 };
