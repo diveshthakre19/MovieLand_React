211f2b1b5c213f153368178089f332398bca69e6
diveshthakre19 <diveshthakre07@gmail.com>
2022-12-29 22:01:59 +0530

    fixed bugs

--- src/app.js
+++ src/app.js
@@ -2,20 +2,19 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import MovieCard from "./MovieCard";
 import SearchIcon from "./search.svg";
-
 const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=b723afc7";
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSerchTerm] = useState(" ");
-
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
+
     setMovies(data.Search);
   };
 
   useEffect(() => {
-    searchMovies("spiderman");
+    searchMovies("Avengers");
   }, []);
 
   return (
@@ -30,19 +29,19 @@ const App = () => {
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => searchMovies({ searchTerm })}
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      {movies.length > 0 ? (
-        <div className="conatainer">
+      {movies?.length > 0 ? (
+        <div className="container">
           {movies.map((movie) => (
             <MovieCard movie={movie} />
           ))}
         </div>
       ) : (
         <div className="empty">
-          <h1>No movie Found</h1>
+          <h2>No movies Found</h2>
         </div>
       )}
     </div>
